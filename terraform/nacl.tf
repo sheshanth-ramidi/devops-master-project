02062df36af3f10004e12f0a8ef2c2a7be2e73b4
sheshanth-ramidi <r.sheshanthr@gmail.com>
2026-01-18 12:48:04 +0000

    updated terraform file

--- terraform/nacl.tf
+++ terraform/nacl.tf
@@ -1,6 +1,7 @@
 resource "aws_network_acl" "public_nacl" {
   vpc_id = aws_vpc.main.id
  
+  # Inbound: Allow HTTP
   ingress {
     rule_no    = 100
     protocol   = "tcp"
@@ -10,6 +11,7 @@ resource "aws_network_acl" "public_nacl" {
     to_port    = 80
   }
  
+  # Inbound: Allow HTTPS
   ingress {
     rule_no    = 110
     protocol   = "tcp"
@@ -19,6 +21,7 @@ resource "aws_network_acl" "public_nacl" {
     to_port    = 443
   }
  
+  # Inbound: Allow SSH
   ingress {
     rule_no    = 120
     protocol   = "tcp"
@@ -28,6 +31,17 @@ resource "aws_network_acl" "public_nacl" {
     to_port    = 22
   }
  
+  # Inbound: Allow Ephemeral Ports (CRITICAL for apt-get/internet response)
+  ingress {
+    rule_no    = 140
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+ 
+  # Egress: Allow ALL Outbound
   egress {
     rule_no    = 100
     protocol   = "-1"
--- terraform/security.tf
+++ terraform/security.tf
@@ -2,30 +2,35 @@ resource "aws_security_group" "devops_sg" {
   name        = "devops-sg"
   description = "Allow SSH and HTTP"
   vpc_id      = aws_vpc.main.id
- 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
  
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
   tags = {
     Name = "devops-sg"
   }
 }
- 
